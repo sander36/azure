8b112862a1b982557cb5893f393c2c5f17d9455e
SanderS <92013809+sander36@users.noreply.github.com>
2023-10-23 09:53:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,6 @@
 
     <p>See on meie veebileht.</p>
 
-    <?php
-        // PHP code to display the current date and time
-        $currentDateTime = date('Y-m-d H:i:s');
-        echo "<p>Current Date and Time: $currentDateTime</p>";
-    ?>
     <img src="https://i.ytimg.com/vi/8Q26YeiYMHM/hqdefault.jpg"
 </body>
 </html>
