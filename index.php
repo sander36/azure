2154fa8b0d7fe9f1d2c802c576a94c35ec959d58
SanderS <92013809+sander36@users.noreply.github.com>
2023-10-23 09:50:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>PHP 8.0 Web Page</title>
+    <title>revonsander</title>
 </head>
 <body>
-    <h1>Hello, World!</h1>
+    <h1>Tere!</h1>
 
-    <p>This is a basic PHP 8.0 web page.</p>
+    <p>See on meie veebileht.</p>
 
     <?php
         // PHP code to display the current date and time
         $currentDateTime = date('Y-m-d H:i:s');
         echo "<p>Current Date and Time: $currentDateTime</p>";
     ?>
+    <img src="https://i.pinimg.com/736x/00/af/60/00af601b947285d31fa1ba13d6e89d78.jpg"
 </body>
 </html>
